4d52a4fb3069509aee380e2fab748b5cd70c49a4
Samylle Carvalho <samylliribeiro2018@gmail.com>
2026-04-07 18:11:22 -0300

    agora vai meu portifólio

---
+++ script.js
@@ -0,0 +1,62 @@
+// lista de projetos
+const MeusProjetos =[
+    {
+        titulo: "Projeto 1",
+        descricao: "meu primeiro projeto",
+        link: "#"
+    },
+    {
+        titulo: "Projeto 2",
+        descricao: "meu segundo projeto",
+        link: "#"
+    },
+    {
+        titulo: "Projeto 3",
+        descricao: "meu terceiro projeto",
+        link: "#"
+    }
+];
+// Gerar os cards(caixinhas) dos projetos dinamicamente
+const container =document.querySelector(".container-meus-projetos");
+//laço de repetição para criar os cards dos projetos sem precisar escrever o código HTML para cada um deles, tornando o processo mais eficiente e organizado.
+MeusProjetos.forEach(projeto => {
+//cria o elemento para cada projeto, adiciona as informações do projeto e o link para visualização dos projetos.    
+    const projetoElement = document.createElement("div");
+//adiciona a classe de estilização css.   
+    projetoElement.classList.add("projeto-card");
+//adiciona o conteudo html para cada projeto, utilizando as informações do objeto projeto.    
+    projetoElement.innerHTML = `
+        <h3>${projeto.titulo}</h3>
+        <p>${projeto.descricao}</p>
+        <a href="${projeto.link}"
+        class="ver-projeto">Ver Projeto</a>`;
+//adiciona o card pronto para o container dos projetos.        
+        container.appendChild(projetoElement);
+    });
+// lista de contatos
+    const Contatos =[
+        {
+            titulo: "WhatsApp",
+            link: "https://wa.me/5586981774652"
+        },
+        {
+            titulo: "E-mail",
+            link: "mailto:samylliribeiro2018@gmail.com"
+        },
+        {
+            titulo: "GitHub",
+            link: "https://github.com/samylleribeiro"
+        }
+    ]
+// Gerar os botões de contato dinamicamente.    
+    const containerContato = document.querySelector(".container-contato");
+//laço de repetição para criar os botões de contato sem precisar escrever o código HTML para cada um deles.    
+    Contatos.forEach(item => {
+//cria o elemento para cada contato, adiciona as informações do contato e o link para visualização dos contatos.        
+        const botao = document.createElement("a");
+        botao.href = item.link;
+        botao.target = "_blank";
+        botao.textContent = item.titulo;
+        botao.classList.add("botao-contato");
+        containerContato.appendChild(botao);
+    });

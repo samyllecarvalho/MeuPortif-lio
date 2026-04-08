2479ffb3140a763982bf125200ce98413ccd0074
Samylle Carvalho <samylliribeiro2018@gmail.com>
2026-04-08 10:49:58 -0300

    ajuste na sintaxe do innerHTML e fechamento do crase

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ MeusProjetos.forEach(projeto => {
         <h3>${projeto.titulo}</h3>
         <p>${projeto.descricao}</p>
         <a href="${projeto.link}"
-        class="ver-projeto" target="_blank">Ver Projeto</a>`;
+        class="ver-projeto" target="_blank">Ver Projeto</a>
+        `;
 //adiciona o card pronto para o container dos projetos.        
         container.appendChild(projetoElement);
     });
